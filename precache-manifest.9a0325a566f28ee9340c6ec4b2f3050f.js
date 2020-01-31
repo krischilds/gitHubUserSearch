12d363cda30eb1ef22844e49f6c2e5cba1a03364
Kris Childs <kris.childs@gmail.com>
2020-01-30 22:16:08 -0800

    Updates

---
+++ precache-manifest.9a0325a566f28ee9340c6ec4b2f3050f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c4a79b246de3c09a08ab8e0d3b09899",
+    "url": "/gitHubUserSearch/index.html"
+  },
+  {
+    "revision": "28e6807abc00b36b510c",
+    "url": "/gitHubUserSearch/static/css/2.0996cbbd.chunk.css"
+  },
+  {
+    "revision": "eb69f66fc79d9b7ad933",
+    "url": "/gitHubUserSearch/static/css/main.07a718d4.chunk.css"
+  },
+  {
+    "revision": "28e6807abc00b36b510c",
+    "url": "/gitHubUserSearch/static/js/2.833caf29.chunk.js"
+  },
+  {
+    "revision": "eb69f66fc79d9b7ad933",
+    "url": "/gitHubUserSearch/static/js/main.5821365d.chunk.js"
+  },
+  {
+    "revision": "449f2e8a38e5d13ecd74",
+    "url": "/gitHubUserSearch/static/js/runtime~main.cce4d3df.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/gitHubUserSearch/precache-manifest.9a0325a566f28ee9340c6ec4b2f3050f.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/gitHubUserSearch/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
